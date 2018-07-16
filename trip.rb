975339adb71c071857953aae56ae1abc77b74b44
lalahang <lylahang086@gmail.com>
2018-07-16 20:03:13 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -7,10 +7,17 @@ puts "Let's plan your trip! Where would you like to stay?"
 
 stay = gets.chomp.capitalize  #don't forget to Capitilize the input
 
+eats = gets.chomp.capitalize
+
+nights = gets.chomp.capitalize
+
+
+
+
 # Do the same thing with `eats` and one last time with `nights`, capturing their text input and storing them to the respective local variable
 
 
 
 # Lastly, puts it all back by interpolating these values in a string.
 
-puts ""
+puts "#{"
